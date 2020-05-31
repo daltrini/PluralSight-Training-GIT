4d11416619ead0ba506145b4d712516fe8a9ebbf
Alex Daltrini <daltrini@gmail.com>
2020-05-31 16:29:31 -0500

    my second commit

---
+++ test.py
@@ -0,0 +1,14 @@
+'''Testing complex expressions
+
+Usage:
+    TBD
+'''
+
+from pprint import pprint as pp
+import os
+import glob
+
+file_sizes = {os.path.realpath(p): os.stat(p).st_size for p in glob.glob('*.py')}
+pp(file_sizes)
+
+# Testing diff on git - PluralSight Training
